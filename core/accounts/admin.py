3b258f56f532489931ea355b4199315808b7b112
Amirhesam <amirhesamshokri@gmail.com>
2023-10-22 23:16:49 +0330

    update accounts/admin.py
    create django admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        ('authentication', {
+            "fields": (
+                'email', 'password'
+                ),
+        }),
+        ('Permissions', {
+            "fields": (
+                "is_staff", "is_active", "groups", "user_permissions"
+                ),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff","is_active","is_superuser"
+                ),
+        }),
+    )
+
+admin.site.register(User , CustomUserAdmin)
