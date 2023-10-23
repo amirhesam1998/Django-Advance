c7e197874dc8799c31640b3e675080bcc2b7b999
Amirhesam <amirhesamshokri@gmail.com>
2023-10-23 23:12:49 +0330

    RedirectView
    Create Rediretiew

--- core/blog/url.py
+++ core/blog/url.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import indexview , Indexview
+from .views import  Indexview , RedirectToMaktab
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path("fbv-index", indexview , name="fbv-index"),
+    #path("fbv-index", indexview , name="fbv-index"),
     #path("cbv-index", TemplateView.as_view(template_name="index.html", extra_context = {"name" : "ali"})),
-    path("cbv-index", Indexview.as_view() , name = "cbv-index")
+    path("cbv-index", Indexview.as_view() , name = "cbv-index"),   #run IndexView  classes
+    path("go-to-varzesh3" , RedirectView.as_view(url = "https://varzesh3.com/") , name="redirect-to-varzesh3.com" ),  
+    path("go-to-redirect-index" , RedirectView.as_view(pattern_name = "blog:cbv-index") , name="redirect-to-cbv-index" ), #go to blog app in open cbv-index path name
+    path("go-to-maktab/<int:pk>" , RedirectToMaktab.as_view() , name="redirect-to-maktabkhone" ),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,9 +1,12 @@
 from typing import Any
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView , RedirectView
 from .models import Post
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
+#function based view to show template (example)
+'''
 def indexview(request) :
     """
     function based view (fbv)
@@ -11,7 +14,7 @@ def indexview(request) :
     name = "ali"
     context = {"name" : name}
     return render(request , 'index.html' , context)
-
+'''
 class Indexview(TemplateView):
     """
     class based view (cbv)
@@ -24,3 +27,25 @@ class Indexview(TemplateView):
         context["name"] = "ali"
         context["posts"] = Post.objects.all()
         return context
+
+
+''' fbv for redirect
+
+from django.shortcuts import redirect
+def redirectToMaktab(request):
+    return redirect("https://maktabkhooneh.com")
+
+'''
+
+
+'''
+ cbv for redirect
+'''
+
+class RedirectToMaktab(RedirectView):
+    url = "https://maktabkhooneh.com"
+
+    def get_redirect_url(self, *args, **kwargs):         # for example if run this url 'blog/go-to-maktab/1' Along with this URL, the post information along with the ID given by the user in the blog application will be transferred to site maktabkhone
+        post = get_object_or_404(Post, pk=kwargs["pk"])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
