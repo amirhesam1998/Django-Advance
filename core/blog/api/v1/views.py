29dd3ae9ba8f5c070a7652235856b104b6b9c958
Amirhesam <amirhesamshokri@gmail.com>
2023-11-01 22:21:09 +0330

    Update Api code (exist model or not exist)

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
@@ -10,7 +12,21 @@ def postList(request):
 
 @api_view()
 def postDetail(request,id):
-    post = Post.objects.get(pk=id)
-    serializer = PostSerializer(post)            #get post and put in the PostSerializer
-    data = serializer.data                     #convert object to json
+    '''
+    solution 1:
+    '''
+    #try:
+        #post = Post.objects.get(pk=id)
+        #serializer = PostSerializer(post)            #get post and put in the PostSerializer
+        #data = serializer.data                     #convert object to json
+        #return Response(data)
+    #except Post.DoesNotExist:
+        #return Response({"detail" : "Post Does not exist"} , status=status.HTTP_404_NOT_FOUND)
+    '''
+    solution 2:
+    '''
+    post = get_object_or_404(Post , pk=id)
+    serializer = PostSerializer(post) 
+    data = serializer.data
     return Response(data)
+
