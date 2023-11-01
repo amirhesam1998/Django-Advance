28eb97b6f7361eb23f3f579b2f7055ae4dd32dfc
Amirhesam <amirhesamshokri@gmail.com>
2023-11-01 21:36:33 +0330

    example api

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import postList
+from .views import postList , postDetail
 
 app_name = "api-v1"
 
 urlpatterns = [
     path('post/',postList,name="post-list"),
+    path('post/<int:id>/',postDetail,name="post-detail"),
 ]
