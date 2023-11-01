7576026bf18f74b0366e870fc28f89d22c65fd02
Amirhesam <amirhesamshokri@gmail.com>
2023-11-01 22:37:15 +0330

    ModelSerializers

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
+from ...models import Post
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 255)
+#class PostSerializer(serializers.Serializer):                               #serializer
+    #id = serializers.IntegerField()
+    #title = serializers.CharField(max_length = 255)
+
+
+class PostSerializer(serializers.ModelSerializer):                           #ModelSerializer
+    class Meta:
+        model = Post
+        fields = "__all__"
