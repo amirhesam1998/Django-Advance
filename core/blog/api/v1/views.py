f6371e62215b528d33e99c39328220c329089dd0
Amirhesam <amirhesamshokri@gmail.com>
2023-11-05 19:20:36 +0330

    change fbv to cbv

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
-"""FBV method :"""
+#Example FBV :
 """@api_view(["GET","POST"])
 @permission_classes([IsAuthenticated])
 def postList(request):
@@ -21,28 +21,8 @@ def postList(request):
         serializer.is_valid(raise_exception=True)                      #validations
         serializer.save()                                              #save serializer
         return Response(serializer.data)"""   
-                                
 
-"""CBV method:"""
-class PostList(APIView):
-    """getting a list of post and creating a new post"""
-    def get(self,request):
-        """retrieving a list of post"""
-        posts = Post.objects.filter(status = True)
-        serializer = PostSerializer(posts , many = True)
-        data = serializer.data
-        return Response(data)
-    def post(self,request):
-        """creating post with provided data"""
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)  
-        serializer.save()
-        return Response(serializer.data)   
-
-
-
-
-@api_view(["GET" , "PUT" , "DELETE"])
+"""@api_view(["GET" , "PUT" , "DELETE"])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def postDetail(request,id):
     '''
@@ -70,5 +50,48 @@ def postDetail(request,id):
         return Response(serializer.data)
     elif request.method == "DELETE":
         post.delete()
-        return Response({"detail" : "item delete successfully"} , status=status.HTTP_204_NO_CONTENT)
+        return Response({"detail" : "item delete successfully"} , status=status.HTTP_204_NO_CONTENT)"""
+
+
+#EXAMPLE CBV:
+class PostList(APIView):
+    """getting a list of post and creating a new post"""
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    def get(self,request):
+        """retrieving a list of post"""
+        posts = Post.objects.filter(status = True)
+        serializer = PostSerializer(posts , many = True)
+        data = serializer.data
+        return Response(data)
+    def post(self,request):
+        """creating post with provided data"""
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)  
+        serializer.save()
+        return Response(serializer.data)   
+
+
+class PostDetail(APIView):
+    """getting detail of the post and edit plus delete the posts"""
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    def get(self,request,id):
+        """retrieving a detail of post"""
+        post = get_object_or_404(Post , pk=id , status = True)
+        serializer = PostSerializer(post) 
+        data = serializer.data
+        return Response(data)
+    def put(self,request,id):
+        """editing the post data"""
+        post = get_object_or_404(Post , pk=id , status = True)
+        serializer = PostSerializer(post , data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)      
+    def delete(self,request,id):
+        """deleting the post """
+        post = get_object_or_404(Post , pk=id , status = True)
+        post.delete()
+        return Response({"detail" : "item delete successfully"} , status=status.HTTP_204_NO_CONTENT)                  
 
