f230bda20f291ddbec587d58f02ed5bd736b9078
Amirhesam <amirhesamshokri@gmail.com>
2023-11-05 19:00:37 +0330

    CBV for API

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import postList , postDetail
+from .views import PostList , postDetail
 
 app_name = "api-v1"
 
 urlpatterns = [
-    path('post/',postList,name="post-list"),
+    #path('post/',postList,name="post-list"),
+    path('post/',PostList.as_view(),name="post-list"),
     path('post/<int:id>/',postDetail,name="post-detail"),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -5,8 +5,10 @@ from .serializers import PostSerializer
 from ...models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
-@api_view(["GET","POST"])
+"""FBV method :"""
+"""@api_view(["GET","POST"])
 @permission_classes([IsAuthenticated])
 def postList(request):
     if request.method == "GET":
@@ -18,8 +20,26 @@ def postList(request):
         serializer = PostSerializer(data=request.data)               #request.data means,show data information send from client to server
         serializer.is_valid(raise_exception=True)                      #validations
         serializer.save()                                              #save serializer
-        return Response(serializer.data)   
+        return Response(serializer.data)"""   
                                 
+
+"""CBV method:"""
+class PostList(APIView):
+    """getting a list of post and creating a new post"""
+    def get(self,request):
+        """retrieving a list of post"""
+        posts = Post.objects.filter(status = True)
+        serializer = PostSerializer(posts , many = True)
+        data = serializer.data
+        return Response(data)
+    def post(self,request):
+        """creating post with provided data"""
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)  
+        serializer.save()
+        return Response(serializer.data)   
+
+
 
 
 @api_view(["GET" , "PUT" , "DELETE"])
