3e8c805daf28728758c338bfd0d080b656f68ff5
Amirhesam <amirhesamshokri@gmail.com>
2023-11-06 19:30:04 +0330

    Show category name instead category id
     def to_representation

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -10,6 +10,7 @@ class PostSerializer(serializers.ModelSerializer):                           #Mo
     snippet = serializers.ReadOnlyField(source = "get_snippet")
     relative_url = serializers.URLField(source = "get_absolute_api_url" , read_only = True)
     absolute_url = serializers.SerializerMethodField(method_name="get_abs_url")
+    category = serializers.SlugRelatedField(many = False , slug_field='name' , queryset = Category.objects.all())
 
     class Meta:
         model = Post
@@ -19,6 +20,9 @@ class PostSerializer(serializers.ModelSerializer):                           #Mo
     def get_abs_url(self,obj):
         request = self.context.get("request")
         return request.build_absolute_uri(obj.pk)
+    
+    def to_representation(self, instance):
+        return super().to_representation(instance)
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
