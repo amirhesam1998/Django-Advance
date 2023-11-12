3a9019d57d52ec186ca14070624c58d684e29e7c
Amirhesam <amirhesamshokri@gmail.com>
2023-11-12 22:47:54 +0330

    setup JWTAuthentication

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,19 +1,32 @@
 from django.urls import path
 from . import views
 #from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 app_name = "api-v1"
 
 urlpatterns = [
     #registrations
     path("registration/",views.RegistrationApiView.as_view(),name = "registration"),
+
+    #login token
     #path('token/login/', ObtainAuthToken.as_view() , name='token-login'),
     path('token/login/' , views.CustomObtainAuthToken.as_view() , name = 'token-login'),
     path('token/logout/' , views.CustomDiscardAuthToken.as_view() , name = 'token-logout'),
-    #set password
-    #reset password
-    #login token
-    #login jwt
 
+    #set password
+
+    #reset password
+
+    #login token
+
+    #login jwt
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
 
 ]
 
--- core/core/settings.py
+++ core/core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "blog",
     "rest_framework",
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     "django_filters",
     "drf_yasg",
 ]
@@ -154,5 +155,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
