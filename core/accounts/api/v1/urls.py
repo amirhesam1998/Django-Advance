39323b67f343797ae649b2ad54aa1a56c308b1ba
Amirhesam <amirhesamshokri@gmail.com>
2023-11-12 21:39:19 +0330

    setup token authentication login

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-
+from rest_framework.authtoken.views import ObtainAuthToken
 app_name = "api-v1"
 
 urlpatterns = [
     #registrations
-    path("registration/",views.RegistrationApiView.as_view(),name = "registration")
+    path("registration/",views.RegistrationApiView.as_view(),name = "registration"),
+    path('token/login', ObtainAuthToken.as_view() , name='token-login'),
     #set password
     #reset password
     #login token
