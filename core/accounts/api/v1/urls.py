9b6bcbe739a4ad698104b7fb939d186eb1d33be1
Amirhesam <amirhesamshokri@gmail.com>
2023-11-13 20:37:21 +0330

    Change password

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -11,20 +11,21 @@ app_name = "api-v1"
 
 urlpatterns = [
     #registrations
+
     path("registration/",views.RegistrationApiView.as_view(),name = "registration"),
 
     #login token
+
     #path('token/login/', ObtainAuthToken.as_view() , name='token-login'),
     path('token/login/' , views.CustomObtainAuthToken.as_view() , name = 'token-login'),
     path('token/logout/' , views.CustomDiscardAuthToken.as_view() , name = 'token-logout'),
 
-    #set password
-
+    #change password
+    path('change-password/' , views.ChangePasswordApiView.as_view() , name = 'change-password'),
     #reset password
 
-    #login token
-
     #login jwt
+
     #path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
     path('jwt/create/', views.CustomTokenObtainPairView.as_view(), name='jwt-create'),
     path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
