d2fc047882c97ed9ab0e017f9f909317fc3ea019
Amirhesam <amirhesamshokri@gmail.com>
2023-11-14 20:47:21 +0330

    send email by smtp4dev

--- core/core/settings.py
+++ core/core/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     "accounts",
     "blog",
     "rest_framework",
-    'rest_framework.authtoken',
-    'rest_framework_simplejwt',
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
     "django_filters",
     "drf_yasg",
 ]
@@ -160,4 +160,10 @@ REST_FRAMEWORK = {
 }
 
 #email configuration
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = False
+EMAIL_HOST = 'smtp4dev'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 25
