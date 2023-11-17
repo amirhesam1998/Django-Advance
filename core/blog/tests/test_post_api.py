e7060f2f590961ccc9938e363c83208711f5edd5
Amirhesam <amirhesamshokri@gmail.com>
2023-11-17 17:20:20 +0330

    test_post_api with pytest

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,6 +1,37 @@
 import pytest
+from rest_framework.test import APIClient
+from django.urls import reverse
+from datetime import datetime
+from accounts.models import User
+@pytest.fixture
+def api_client():
+    client = APIClient()
+    return client        
 
-class TestPostApi():
+
+@pytest.fixture
+def common_user():
+    user = User.objects.create_user(email = 'admin@admin.com' , password = "a/@1234567", is_verified = True)
+    return user
     
-    def test_get_post_response_200_status(self):
-        assert 1==1
+
+@pytest.mark.django_db
+class TestPostApi():
+    def test_get_post_response_200_status(self,api_client):
+        url = reverse('blog:api-v1:post-list')
+        response = api_client.get(url)
+        assert response.status_code == 200
+        
+    def test_create_post_response_201_status(self,api_client,common_user):         
+        url = reverse('blog:api-v1:post-list')
+        data = {
+            'title' :'test',
+            'content': 'description',
+            'status':True,
+            'published_date' : datetime.now()
+        }
+        user = common_user
+        api_client.force_login(user = user)
+        api_client.force_authenticate(user = user)
+        response = api_client.post(url,data)    
+        assert response.status_code == 201       
