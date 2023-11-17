02e10fbcc066c41b9c96fc26b9fd8b3155f881ba
Amirhesam <amirhesamshokri@gmail.com>
2023-11-17 13:50:34 +0330

    test blog url

---
+++ core/blog/tests/test_blog_url.py
@@ -0,0 +1,18 @@
+from django.test import TestCase , SimpleTestCase
+from django.urls import reverse , resolve
+from ..views import Indexview , PostDetailView , PostlistView
+# Create your tests here.
+
+class TestUrl(SimpleTestCase):
+    
+    def test_blog_url_index_resolve(self):
+        url = reverse('blog:fbv-index')
+        self.assertEqual(resolve(url).func.view_class , Indexview)
+        
+    def test_blog_post_detail_url_resolve(self):
+        url = reverse('blog:post-detail',kwargs={'pk':1})
+        self.assertEqual(resolve(url).func.view_class , PostDetailView)
+        
+    def test_blog_post_list_url_resolve(self):
+        url = reverse('blog:post-list')
+        self.assertEqual(resolve(url).func.view_class , PostlistView)
