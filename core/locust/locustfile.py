1ac26d251d7ebebfe77790c031d328f76b93684e
Amirhesam <amirhesamshokri@gmail.com>
2023-11-18 15:08:19 +0330

    install and setup locust and defined any task in config file

---
+++ core/locust/locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    
+    @task
+    def post_list(self):
+        self.client.get("/blog/api/v1/post")
+        
+    @task
+    def category_list(self):
+        self.client.get("/blog/api/v1/category")
