fc2fc4259f90f758ac00eef0c2fb64ca20d83bd9
Amirhesam <amirhesamshokri@gmail.com>
2023-11-18 16:29:13 +0330

    install celery

--- core/core/settings.py
+++ core/core/settings.py
@@ -171,3 +171,10 @@ EMAIL_HOST = "smtp4dev"
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 25
+
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+#celery configs
+CELERY_BROKER_URL = 'redis://redis:6379/1'
