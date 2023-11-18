bfc11cd546cbf0d72923bce11c61876d2b619208
Amirhesam <amirhesamshokri@gmail.com>
2023-11-18 20:36:50 +0330

    create simple caching

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import send_email
-
+from .views import send_email , test
 app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),  # for LoginRequiredMixin
+    path("send_email/", send_email , name="send-email"),
+    path("test/", test , name="test"),
     path("api/v1/", include("accounts.api.v1.urls")),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse , JsonResponse
+import time
+import requests
 # Create your views here.
 
+def send_email(request):
+    time.sleep(3)
+    return HttpResponse('<h1>ok<\h1>')
+
+def test(request):
+    response = requests.get('https://b0d46176-e776-489e-8198-8335c085a042.mock.pstmn.io/test/delay/5').json()
+    return JsonResponse(response)
